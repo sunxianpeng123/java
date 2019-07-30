4aaaa51512340964b0e758fa6f258513fa418ae3
Dell-PC\Dell <sunxianpeng@xiaohulu.com>
2019-07-30 20:03:46 +0800

    update

--- java_study/src/multiThread/base/synchronization/SyncDeadLock.java
+++ java_study/src/multiThread/base/synchronization/SyncDeadLock.java
@@ -15,6 +15,42 @@ package multiThread.base.synchronization;
  */
 public class SyncDeadLock {
     public static void main(String[] args) {
+        new DeadLockThread();
 
     }
+}
+class DeadLockThread implements Runnable{
+    Customer c =new Customer();
+    Waiter w = new Waiter();
+    public DeadLockThread(){
+        new Thread(this).start();
+        w.say(c);
+    }
+    @Override
+    public void run() {
+        c.say(w);
+
+    }
+}
+class Customer{
+    public synchronized  void  say(Waiter w){
+        System.out.println("顾客说，先吃饭再买单！！");
+        w.doService();
+    }
+
+    public synchronized void doService(){
+        System.out.println("同意了，买完单再吃饭！！");
+    }
+}
+
+class Waiter{
+    public synchronized  void  say(Customer c){
+        System.out.println("服务员说，先吃饭再买单！！");
+        c.doService();
+    }
+
+    public synchronized void doService(){
+        System.out.println("同意了，吃完饭再买单！！");
+    }
+
 }
